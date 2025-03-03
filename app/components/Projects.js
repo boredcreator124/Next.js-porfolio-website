cdab8b47ea77d69f9ef276b97af99348103671b4
Jacob Baddoo <jacobniilanteibaddoo6@gmail.com>
2025-03-03 14:03:18 +0000

    Update Projects.js

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -33,7 +33,7 @@ const Projects = () => {
       <div className="max-w-6xl mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {projectsData.map((project, index) => (
           <div key={index} className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <img src={project.image} alt={project.title} className="rounded-lg mb-4" />
+          <alt={project.title} className="rounded-lg mb-4" />
             <h3 className="text-2xl font-semibold">{project.title}</h3>
             <p className="text-gray-400 mt-2">{project.description}</p>
             <div className="mt-3">
